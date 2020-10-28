2024199d170684ac44e4b0fa469347f63b85b0cd
Gabriela Zalta <gzalta96@gmail.com>
2020-10-28 09:39:21 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,14 +27,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
             window.alert (playerName + " has decided to skip this fight. Goodbye!");
             //subtract money from playerMoney for skipping
-            playerMoney= playerMoney - 10;
+            playerMoney= Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
             }
         }
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log (
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
         );
@@ -53,7 +54,8 @@ var fight = function(enemyName) {
          }
 
         // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log (
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
         );
@@ -83,7 +85,7 @@ var startGame = function () {
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyName = enemyNames[i];
             // reset enemyHealth before starting new fight
-            enemyHealth= 50;
+            enemyHealth= randomNumber (40, 60);
             // call fight function with enemy-robot
             fight(pickedEnemyName);
             // if player is still alive and we're not at the last enemy in the array
@@ -169,6 +171,13 @@ var shop = function () {
                 shop ();
                 break;
     }
+};
+
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min * 1) + min);
+
+    return value;
 };
 
 //start the game when the page loads
