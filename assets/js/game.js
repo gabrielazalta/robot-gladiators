970375554369ffd5dd09b39319d98ca7e0cd76e5
Gabriela Zalta <gzalta96@gmail.com>
2020-10-29 10:37:39 -0500

    randomize order in fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,8 +35,14 @@ var fightOrSkip = function() {
   }
 
 var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if (Math.random () > 0.5) {
+        isPlayerTurn = false;
+    }
     // repeat and execute as long as the enemy-robot is alive 
     while (enemy.health > 0 && playerInfo.health > 0) {
+        if (isPlayerTurn) {
         if (fightOrSkip()) {
             // if true, leave fight by breaking loop
             break;
@@ -51,16 +57,14 @@ var fight = function(enemy) {
         // check enemy's health
         if (enemy.health <= 0) {
             window.alert (enemy.name + " has died!");
-
             //award player money for winning
             playerInfo.money = playerInfo.money + 20;
-
             // leave while() loop since enemy is dead
             break;
         } else {
             window.alert (enemy.name + " still has " + enemy.health + " health left.");
-         }
-
+        }
+        } else {
         // remove player's health by subtracting the amount set in the enemyAttack variable
         var damage = randomNumber(enemy.attack - 3, enemy.attack);
         playerInfo.health = Math.max(0, playerInfo.health - damage);
@@ -70,12 +74,15 @@ var fight = function(enemy) {
 
         // check players health
         if (playerInfo.health <= 0) {
-        window.alert (playerInfo.name + " has died!");
-        // leave while() loop if player is dead
-        break;
+            window.alert (playerInfo.name + " has died!");
+            // leave while() loop if player is dead
+            break;
         } else {
             window.alert (playerInfo.name + " still has " + playerInfo.health + " health left.");
+         }
         }
+        //switch turn order for next round
+        isPlayerTurn = !isPlayerTurn;
     }
 };
 
